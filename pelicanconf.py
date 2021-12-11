aa259e781bf5b0201ba88968a6481287d13d3dbb
Aayush Shah <shahaayush349@gmail.com>
2021-12-12 00:02:42 +0530

    apps section added

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,19 @@ DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# choosing theme from my available Themes
+THEME = 'Flex'
+#THEME = 'notmyidea'
+#THEME = 'pelican-themes'
+#THEME = 'simple'
+#THEME = 'nice-blog'
+#THEME = 'attila'
+
+# Theme switching
+THEME_COLOR = 'dark'
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+PYGMENTS_STYLE = 'emacs'
+PYGMENTS_STYLE_DARK = 'monokai'
